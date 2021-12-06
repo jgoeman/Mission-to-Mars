48019d0d935d3017024c1fdb2d7889b3aecf46a3
Joe Goeman <jgoeman86@gmail.com>
2021-12-05 20:24:47 -0600

    upload completed challenge

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
 
@@ -11,7 +11,7 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
     mars = mongo.db.mars.find_one()
-    return render_template("index.html"), mars=mars)
+    return render_template("index.html", mars=mars)
 
 @app.route("/scrape")
 def scrape():
@@ -21,4 +21,4 @@ def scrape():
     return redirect('/', code=302)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- scraping.py
+++ scraping.py
@@ -19,7 +19,8 @@ def scrape_all():
       "news_paragraph": news_paragraph,
       "featured_image": featured_image(browser),
       "facts": mars_facts(),
-      "last_modified": dt.datetime.now()
+      "last_modified": dt.datetime.now(),
+      "hemispheres": mars_hemispheres(browser)
     }
     
     # Stop webdriver and return data
@@ -92,6 +93,29 @@ def mars_facts():
     # Convert the new df to html
     return df.to_html()
 
+def mars_hemispheres(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+
+# 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    links = browser.find_by_css('a.product-item img')
+
+    for i in range(len(links)):
+        hemishpere = {}
+        browser.find_by_css('a.product-item img')[i].click()
+        sample_elem = browser.links.find_by_text('Sample').first
+        hemishpere['img_url'] =  sample_elem['href']
+        hemishpere['title'] = browser.find_by_css('h2.title').text
+        hemisphere_image_urls.append(hemishpere)
+        browser.back()
+
+    return hemisphere_image_urls
+
 if __name__ == "__main__":
     # If running as script, print scraped data
     print(scrape_all())
+
